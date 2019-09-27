6cebff9a94e643267573ad6647c3ee65d65b56e6
Priyanka Sariya <priyanka.sariya@gmail.com>
2019-09-27 14:28:00 -0400

    commit

--- 2019_Fall_Assignment2/Program.cs
+++ 2019_Fall_Assignment2/Program.cs
@@ -31,7 +31,8 @@ namespace _2019_Fall_Assignment2
             //Display2DArray(flipAndInvertedImage);
             //Debug.Write("\n");
 
-            int[,] intervals = { { 0, 30 }, { 15, 20 }, { 5, 10 }};
+            //int[,] intervals = { { 0, 30 }, { 5, 10 }, { 15, 20 }};
+            int[,] intervals = { { 7, 10 }, { 2, 4 } };
             int minMeetingRooms = MinMeetingRooms(intervals);
             Debug.WriteLine("Minimum meeting rooms needed = {0}\n", minMeetingRooms);
 
@@ -153,7 +154,7 @@ namespace _2019_Fall_Assignment2
             try
             {
                 //Sort the 2D array
-                for (int i = 0; i < intervals.GetLength(0); i++)
+                for (int i = 0; i < intervals.GetLength(0) - 1; i++)
                 {
                     //Debug.WriteLine("loop 1 " + i);
                     for (int j = intervals.GetLength(1) - 1; j > 0; j--)
@@ -162,18 +163,26 @@ namespace _2019_Fall_Assignment2
                         for (int k = 0; k < j; k++)
                         {
                             //Debug.WriteLine("loop 3 " + k);
-                            if (intervals[i, k] > intervals[i, k + 1])
+                            //if (intervals[i, k] > intervals[i, k + 1])
+                            if (intervals[i, k] > intervals[i + 1, k])
                             {
                                 //Debug.WriteLine("if statement ");
-                                int myTemp = intervals[i, k];
-                                intervals[i, k] = intervals[i, k + 1];
-                                intervals[i, k + 1] = myTemp;
+                                int myTemp1 = intervals[i, k];
+                                intervals[i, k] = intervals[i + 1, k];
+                                intervals[i + 1, k] = myTemp1;
+
+                                int myTemp2 = intervals[i, k + 1];
+                                intervals[i, k + 1] = intervals[i + 1, k + 1];
+                                intervals[i + 1, k + 1] = myTemp2;
                             }
                         }
                     }
                 }
                 //Print the 2D array
-                //Debug.Write("sorted array ");
+                //Debug.WriteLine("sorted array " + intervals.GetLength(0) + " " + intervals.GetLength(1));
+                //Debug.WriteLine(intervals[0, 0] + " " + intervals[0, 1]);
+                //Debug.WriteLine(intervals[1, 0] + " " + intervals[1, 1]);
+                //Debug.WriteLine(intervals[2, 0] + " " + intervals[2, 1]);
                 //for (int x = 0; x < intervals.GetLength(0); x++)
                 //{
                 //    for (int y = 0; y < intervals.GetLength(1); y++)
@@ -181,7 +190,30 @@ namespace _2019_Fall_Assignment2
                 //        Debug.Write(intervals[x, y] + "\t");
                 //    }
                 //}
+                int meetingRooms = 1; //because we will need atleast 1 room for the meetings
+                for (int i = 0; i < intervals.GetLength(0) - 1; i++)
+                {
+                    Debug.WriteLine("loop 1 " + i);
+                    for (int j = intervals.GetLength(1) - 1; j > 0; j--)
+                    {
+                        Debug.WriteLine("loop 2 " + j);
+                        for (int k = 0; k < j; k++)
+                        {
+                            Debug.WriteLine("loop 3 " + k);
+                            if (intervals[i, k] < intervals[i + 1, k]) //if start time of meeting2 is after meeting1
+                            {
+                                Debug.WriteLine("if start time of meeting2 is after meeting1");
+                                if ((intervals[i, k + 1] > intervals[i + 1, k + 1])) //and if endtime if meeting2 is before meeting1
+                                {
+                                    Debug.WriteLine("and if endtime if meeting2 is before meeting1");
+                                    meetingRooms += 1; //we will need one more meeting room due to timing overlap.
+                                }
 
+                            }
+                        }
+                    }
+                }
+                Debug.WriteLine("Number of Meeting Rooms " + meetingRooms);
             }
             catch (Exception ex)
             {
